806a7a26469124b6ace350f9ebbf1508b7f6d447
OpenWh <310784173@qq.com>
2016-09-30 10:46:34 +0800

    2016930

--- H5DrawGraph/H5DrawGraph/Javascript/DrawGraphics.js
+++ H5DrawGraph/H5DrawGraph/Javascript/DrawGraphics.js
@@ -2,6 +2,9 @@
 
 DrawGraphics.Coordinate = {
     createCoordinate: function (x, y) {
+        var myCanvas = document.getElementById("myCanvas");
+        x = x - myCanvas.offsetLeft;
+        y = y - myCanvas.offsetTop;
         var coordinate = {
             _x: x,
             _y: y
@@ -11,16 +14,6 @@ DrawGraphics.Coordinate = {
 }
 
 DrawGraphics.DrawTools = {
-    drawConnectLine: function (myCanvas, cxt, nowcoordinate, nextcoordinate) {
-        if (nowcoordinate !== undefined && nextcoordinate != undefined) {
-            if (nowcoordinate !== nextcoordinate) {
-                cxt.beginPath();
-                cxt.moveTo(nowcoordinate._x, nowcoordinate._y);
-                cxt.lineTo(nextcoordinate._x, nextcoordinate._y);
-                cxt.stroke();
-            }
-        }
-    },
     drawLine: function (myCanvas, cxt, coordinate1, coordinate2) {
         if (coordinate1 !== null && coordinate2 != null) {
             cxt.beginPath();
@@ -35,6 +28,52 @@ DrawGraphics.DrawTools = {
             cxt.rect(coordinate1._x, coordinate1._y, coordinate2._x - coordinate1._x, coordinate2._y - coordinate1._y);
             cxt.stroke();
         }
+    },
+    drawConnectLine: function (myCanvas, cxt, nowcoordinate, nextcoordinate) {
+        if (nowcoordinate !== undefined && nextcoordinate != undefined) {
+            if (nowcoordinate !== nextcoordinate) {
+                cxt.beginPath();
+                cxt.moveTo(nowcoordinate._x, nowcoordinate._y);
+                cxt.lineTo(nextcoordinate._x, nextcoordinate._y);
+                cxt.stroke();
+            }
+        }
+    }
+}
+
+DrawGraphics.utilityTools = {
+    addMouseDownEvent: function (myCanvas, cxt) {
+        document.getElementById("ConnectLineCheckBox").onclick = function () {
+            myCanvas.onmousedown = null;
+            myCanvas.onmouseup = null;
+            if (this.checked) {
+                MouseDown(myCanvas, cxt);
+            }
+        }
+    },
+    addMouseDownToUpEvent: function (myCanvas, cxt, coordinate1, coordinate2) {
+        $(".drawTools").click(function () {
+            var tag = this.getAttribute("tag");
+            if (tag === "drawConnectLine")
+                return;
+            myCanvas.onmousedown = null;
+            myCanvas.onmouseup = null;
+            if (this.checked) {
+                MouseDownToUp(this.getAttribute("tag"), myCanvas, cxt, coordinate1, coordinate2);
+            }
+        });
+    },
+    showPosition: function () {
+        var body = $('body');
+        body.append('<label id="ShowXY"></label>')
+        var showXY = document.getElementById("ShowXY");;
+        myCanvas.onmousemove = function (e) {
+            showXY.innerHTML = (e.pageX - myCanvas.offsetLeft) + "," + (e.pageY - myCanvas.offsetTop);
+            showXY.style.position = "absolute";
+            showXY.style.zIndex = -999;
+            showXY.style.left = e.pageX + 'px';
+            showXY.style.top = (e.pageY - 15) + 'px'
+        }
     }
 }
 
@@ -47,25 +86,12 @@ window.onload = function () {
 
     document.getElementById("LineCheckBox").setAttribute("checked", "checked");
     MouseDownToUp(method, myCanvas, cxt, coordinate1, coordinate2);
-
-    $(".drawTools").click(function () {
-        myCanvas.onmousedown = null;
-        myCanvas.onmouseup = null;
-        if (this.checked) {
-            MouseDownToUp(this.getAttribute("tag"), myCanvas, cxt, coordinate1, coordinate2);
-        }
-    });
-
-    document.getElementById("ConnectLineCheckBox").onclick = function () {
-        myCanvas.onmousedown = null;
-        myCanvas.onmouseup = null;
-        if (this.checked) {
-            MouseDown(this.getAttribute("tag"), myCanvas, cxt, coordinate1);
-        }
-    }
+    DrawGraphics.utilityTools.showPosition();
+    DrawGraphics.utilityTools.addMouseDownEvent(myCanvas, cxt);
+    DrawGraphics.utilityTools.addMouseDownToUpEvent(myCanvas, cxt, coordinate1, coordinate2);
 }
 
-function MouseDown(method, myCanvas, cxt, coordinate1) {
+function MouseDown(myCanvas, cxt) {
     var addCoordinate = DrawGraphics.Coordinate.createCoordinate;
     var nowcoordinate;
     myCanvas.onmousedown = function (e) {
